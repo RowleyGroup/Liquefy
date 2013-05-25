158e79d272a103fb4223c2d535546ec0fd5aa34e
Leif Hickey <leif.hickey@gmail.com>
2013-05-25 14:02:02 -0230

    Initial commit
    Created liquidgen.tcl to provide liquidgen plugin for VMD
    Added initial package requirements

---
+++ liquidgen.tcl
@@ -0,0 +1,11 @@
+# liquidgen plugin for VMD
+# Setup a Molecular Liquid Simulation 
+
+package require psfgen
+package require namdgui
+package provide liquidgen 1.0
+
+# GUI
+# PBC
+# ER
+vmdcon -info "testing"
