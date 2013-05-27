d117b2837a1fb23bbcf5123a1d63f0f8f92b95b1
Leif Hickey <leif.hickey@mun.ca>
2013-05-27 11:52:11 -0230

    Molecules loaded into viewer, some error handling
    Also cleaned up gui code

--- liquify.tcl
+++ liquify.tcl
@@ -1,16 +1,18 @@
 # liquidgen plugin for VMD
 # Setup a Molecular Liquid Simulation 
 
-package require psfgen
-package require namdgui
 package provide liquify 1.0
+#package require psfgen
+#package require namdgui
 
 # Setup namespace to prevent plugin conflicts
 namespace eval ::liquify {
 	#namespace export liquify
+	
 	# window handler
 	variable w
 	set w [toplevel .liquify]
+
 	# Simulation parameters
 	variable options
 	set options(niter) 100
@@ -22,8 +24,9 @@ namespace eval ::liquify {
 		set options($n) 10
 	}
 }
-# GUI
 
+# Build a window to allow user input of parameters
+# $w will be passed by global liquify_tk to VMD
 proc ::liquify::build_gui {} {
 	variable w
 	variable options
@@ -38,66 +41,78 @@ proc ::liquify::build_gui {} {
 	
 	# Frame containing PBD and PSF input fields
 	# PDB File
-	grid [labelframe $w.f1 -text "Load Molecule"] \
-		-columnspan 2 -rowspan 1
-	grid [label $w.f1.l1 -text "PDB File"] -column 0 -row 0
-	grid [entry $w.f1.e1 -width $twidth -textvariable \
-		::liquify::options(pdbfile)] -column 1 -row 0
-	grid [button $w.f1.b1 -text "Browse" -command {set \
-		::liquify::options(pdbfile) [tk_getOpenFile]}] -column 2 -row 0
-	
-	# PSF File
-	grid [label $w.f1.l2 -text "PSF File"] -column 0 -row 1
-	grid [entry $w.f1.e2 -width $twidth -textvariable \
-		::liquify::options(psffile)] -column 1 -row 1
-	grid [button $w.f1.b2 -text "Browse" -command {set \
-		::liquify::options(psffile) [tk_getOpenFile]}] -column 2 -row 1
+	grid [labelframe $w.f1 -text "Load Molecule"] -columnspan 2 -rowspan 1
+
+	set row 0
+	foreach n {pdb psf} {
+		set bigname [string toupper $n]
+		set l [label $w.f1.$n-l1 -text "$bigname File"]
+		set e [entry $w.f1.$n-e1 -width $twidth -textvariable ::liquify::options($n)] 
+		set cmd "set ::liquify::options($n) \[tk_getOpenFile\]"
+		set b [button $w.f1.$n-b1 -text "Browse" -command $cmd]
+		grid $l -column 0 -row $row
+		grid $e -column 1 -row $row
+		grid $b -column 2 -row $row
+		incr row
+	}
 	
 	# Frame containing box dimensions
-	grid [labelframe $w.f2 -text "Box Dimensions"] \
-		-column 0 -row 1
-	grid [entry $w.f2.e1 -textvariable ::liquify::options(x) -width \
-		$nwidth] -column 1 -row 0
-	grid [entry $w.f2.e2 -textvariable ::liquify::options(y) -width \
-		$nwidth] -column 1 -row 1
-	grid [entry $w.f2.e3 -textvariable ::liquify::options(z) -width \
-		$nwidth] -column 1 -row 2
-	grid [label $w.f2.l1 -text "x"] -column 0 -row 0
-	grid [label $w.f2.l2 -text "y"] -column 0 -row 1
-	grid [label $w.f2.l3 -text "z"] -column 0 -row 2
-	grid [label $w.f2.l4 -text "Use Cube"] \
-		-column 2 -row 0
-	grid [checkbutton $w.f2.c1 -variable ::liquify::options(cube)] \
-	-column 3 -row 0
+	grid [labelframe $w.f2 -text "Box Dimensions"] -column 0 -row 1
+	
+	set row 0
+	foreach n {x y z} {
+		set e [entry $w.f2.$n-e1 -textvariable ::liquify::options($n) -width $nwidth]
+		set l [label $w.f2.$n-l1 -text "$n"]
+		grid $l -column 0 -row $row
+		grid $e -column 1 -row $row
+		incr row
+	}
+
+	set l [label $w.f2.l4 -text "Use Cube"]
+	set c [checkbutton $w.f2.c1 -variable ::liquify::options(cube)]
+	grid $l -column 2 -row 0
+	grid $c -column 3 -row 0
 
 	# Frame containing number iterations
-	grid [labelframe $w.f4 -text "Runtime Options"] \
-		-column 1 -row 1
-	grid [label $w.f4.l1 -text "Number of iterations"] \
-		-column 0 -row 0
-	grid [spinbox $w.f4.s1 -textvariable ::liquify::options(niter) \
-		-from 100 -to 1000 -increment 50 -width $nwidth] -column 1 -row 0
-	grid [label $w.f4.l2 -text "Use early rejection"] \
-		-column 0 -row 1
-	grid [checkbutton $w.f4.c1 -variable ::liquify::options(reject)] \
-		-column 1 -row 1
+	grid [labelframe $w.f4 -text "Runtime Options"] -column 1 -row 1
 
-	# Frame containing generate and reset buttons
-	grid [labelframe $w.f5 -text "Populate Box"] -columnspan 2 -rowspan 1
-	grid [button $w.f5.b1 -text "Populate!" -command \
-		{::liquify::populate}]
+	set l [label $w.f4.l1 -text "Number of iterations"]
+	set s [spinbox $w.f4.s1 -textvariable ::liquify::options(niter) \
+		-from 100 -to 1000 -increment 50 -width $nwidth]
+	grid $l -column 0 -row 0
+	grid $s -column 1 -row 0
 
+	set l [label $w.f4.l2 -text "Use early rejection"]
+	set c [checkbutton $w.f4.c1 -variable ::liquify::options(reject)]
+	grid $l -column 0 -row 1
+	grid $c -column 1 -row 1
 
 	# Save new PDB and PSF files
 	# Click button save -> prompt for directory and name
-	grid [labelframe $w.f3 -text "Save New Data"] \
-		-columnspan 2 -rowspan 1
-	grid [label $w.f3.l1 -text "Location"] -column 0 -row 0
-	grid [entry $w.f3.e1 -textvariable ::liquify::options(savedir) \
-		-width $twidth] -column 1 -row 0
-	grid [button $w.f3.b1 -text "Browse" -command {set \
-		::liquify::options(savedir)	[tk_chooseDirectory]}] \
-		-column 2 -row 0
+	grid [labelframe $w.f3 -text "Save New Data"] -columnspan 2 -rowspan 1
+	
+	set l [label $w.f3.l1 -text "Location"] 
+	set e [entry $w.f3.e1 -textvariable ::liquify::options(savedir) -width $twidth] 
+	set cmd "set ::liquify::options(savedir) \[tk_chooseDirectory\]"
+	set b [button $w.f3.b1 -text "Browse" -command $cmd]
+	grid $l -column 0 -row 0
+	grid $e -column 1 -row 0
+	grid $b -column 2 -row 0
+
+	set l [label $w.f3.l2 -text "Name"]
+	set e [entry $w.f3.e2 -textvariable ::liquify::options(savefile) -width $twidth]
+	set cmd "::liquify::save_files"
+	set b [button $w.f3.b2 -text "Save PBD/PSF" -command $cmd]
+	grid $l -column 0 -row 1
+	grid $e -column 1 -row 1
+	grid $b -column 2 -row 1
+	
+	# Frame containing generate and reset buttons
+	grid [labelframe $w.f5 -text "Populate Box"] -columnspan 2 -rowspan 1
+
+	set cmd "::liquify::populate"
+	set b [button $w.f5.b1 -text "Fill!" -command $cmd]
+	grid $b -column 0 -row 0
 }
 
 # temp
@@ -106,16 +121,52 @@ proc ::liquify::print_options {} {
 	parray options
 }
 
+# Save new PDB and PSF files
+proc ::liquify::save_files {} {
+	puts "saving files...maybe"
+}
+
 # Start populating the box
 proc ::liquify::populate {} {
-	puts "Populating!!"
-	print_options
+	vmdcon -info "Reset display field..."
+	::liquify::reset
+	vmdcon -info "Populating..."
+	foreach n {pdb psf} {
+		set bigname [string toupper $n]
+		vmdcon -info "Loading $bigname file..."
+		set err [::liquify::add_mol $::liquify::options($n)]
+		if {$err} {
+			vmdcon -info "Halting box fill!"
+			return 1
+		}
+		vmdcon -info "$bigname loaded successfully"
+	}
+}
+
+# Unload all molecules
+proc ::liquify::reset {} {
+	vmdcon -info "Removing [molinfo num] molecules"
+	set idlist [molinfo list]
+	foreach id $idlist {
+		mol delete $id
+	}
+	vmdcon -info "...done"
 }
 
 # VMD menu calls this function when selected
 proc ::liquify_tk {} {
 	::liquify::build_gui
 	return $liquify::w
+}
+
+proc ::liquify::add_mol {pdbfile} {
+	# Use VMD file handling
+	return [catch {mol addfile $pdbfile} err]
+}
+# Need to validate user input
+proc ::liquify::validate_input {} {
+	variable options
+	puts "Input validation..."
 }
 # PBC
 # ER
