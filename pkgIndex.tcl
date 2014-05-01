9870475f11855b9bcebe744b59bdb305feffb176
Christopher Rowley <cnrowley@gmail.com>
2014-05-01 13:34:25 -0230

    first commit

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -8,4 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded liquify 1.0 [list source [file join $dir liquify.tcl]]
+package ifneeded liquefy 1.0 [list source [file join $dir liquefy.tcl]]
